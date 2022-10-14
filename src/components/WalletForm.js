b19fa86a60f9eb36d3facb09a1c616b034de682b
BradockBr <robtognetti@gmail.com>
2022-10-14 01:43:45 -0300

    requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,11 +1,87 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(getCurrencies());
+  }
+
   render() {
+    const { coins } = this.props;
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="value">
+          <input
+            // name="email"
+            type="number"
+            id="value"
+            // placeholder="email"
+            data-testid="value-input"
+            // onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="description">
+          <input
+            // name="email"
+            type="text"
+            id="description"
+            // placeholder="email"
+            data-testid="description-input"
+            // onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="select">
+          <select
+            // name="email"
+            id="select"
+            data-testid="currency-input"
+            // onChange={ this.handleChange }
+          >
+            {coins?.map((coin, i) => (
+              <option key={ i }>{coin}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="method">
+          <select
+            // name="email"
+            id="method"
+            data-testid="method-input"
+            // onChange={ this.handleChange }
+          >
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          <select
+            // name="email"
+            id="tag"
+            data-testid="tag-input"
+            // onChange={ this.handleChange }
+          >
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
 
-export default WalletForm;
+const mapStateToProps = (state) => ({
+  coins: state.wallet.currencies,
+});
+
+WalletForm.propTypes = {
+  coins: PropTypes.arrayOf(PropTypes.string),
+}.isRequired;
+
+export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,13 @@
 // Coloque aqui suas actions
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
+export const responseAPI = (payload) => ({ type: GET_CURRENCIES, payload });
 export const saveEmail = (payload) => ({ type: SAVE_EMAIL, payload });
+
+export const getCurrencies = () => async (dispatch) => {
+  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const result = await response.json();
+  console.log(result);
+  dispatch(responseAPI(Object.keys(result).filter((coin) => coin !== 'USDT')));
+};
